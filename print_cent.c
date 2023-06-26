770d473e8d5207643db68f35152c52f7819c05ea
MercifulMary <marybakare90@gmail.com>
2023-06-26 16:00:08 -0700

    print_cent.c

---
+++ print_cent.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_cent - print %
+ * @args: unused
+ *
+ * Return: number of characters printed
+ */
+
+int print_cent(va_list args __attribute__((unused)))
+{
+	return (_putchar('%'));
+}
