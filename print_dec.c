198eb26ee74fea6531153524ab3ed9c9fcd76167
MercifulMary <marybakare90@gmail.com>
2023-06-26 16:16:12 -0700

    print_dec.c

---
+++ print_dec.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * print_num_dec - print decimal
+ * @n: decimal number
+ * @count: count
+ *
+ * Return: number of characters printed
+ */
+
+int print_num_dec(int n, int count)
+{
+	if ((n / 10) > 0)
+		count = print_num_dec(n / 10, 0);
+	count += _putchar('0' + n % 10);
+	return (count);
+}
+
+/**
+ * print_dec - print decimal
+ * @args: argument list
+ *
+ * Return: number of characters printed
+ */
+
+int print_dec(va_list args)
+{
+	int n;
+	int count = 0;
+
+	n = va_arg(args, int);
+
+	if (n == 0)
+	{
+		count += _putchar('0');
+		return (count);
+	}
+	if (n < 0)
+	{
+		n = -(n);
+		count += _putchar('-');
+	}
+
+	count += print_num_dec(n, 0);
+	return (count);
+}
+
