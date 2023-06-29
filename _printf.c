ca5053297532a289df270396beabfebc1c7d5492
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2023-06-29 07:30:05 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -28,6 +28,8 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] == '%')
 		{
+			if (format[i + 1] == '\0')
+				break;
 			flags_processed = process_flags(format, i + 1);
 			i = flags_processed->index;
 			func = check_specifier(&format[i]);
